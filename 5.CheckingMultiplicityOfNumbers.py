449a3977dc53ad5458c903719b90c22d09902da9
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-03-05 15:05:18 +0300

    Add correction

--- 5.CheckingMultiplicityOfNumbers.py
+++ 5.CheckingMultiplicityOfNumbers.py
@@ -3,9 +3,9 @@
 print('Введите число')
 number = int(input())
 
-# if (number % 5 == 0 and number % 10 == 0):
-#     print('Число кратно 5 и 10')
-# elif (number % 15 == 0 and number % 30 != 0):   
-#     print('Число кратно 15 и не кратно 30')
-# else:
-#     print('Число не кратно 5, 10 и 15')
+if (number % 5 == 0 and number % 10 == 0):
+    print('Число кратно 5 и 10')
+elif (number % 15 == 0 and number % 30 != 0):   
+    print('Число кратно 15 и не кратно 30')
+else:
+    print('Число не кратно 5, 10 и 15')
