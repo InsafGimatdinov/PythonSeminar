45465129fb314845999789a0b170d5492995aa9b
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-03-21 21:40:58 +0300

    Add correct in Exasmple about Generate List

--- 11.GenerateListOfNumbers.py
+++ 11.GenerateListOfNumbers.py
@@ -1,14 +1,23 @@
 # 11. Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-N = int(input('Write number N: '))
-if N == ' ':
-    print('Incorrect number')
-else:
-    temp = 1
-    lst1 = []
-    lst1.append(temp) 
-    for i in range(N - 1):
-        temp *= -3
-        lst1.append(temp)
-    print(lst1)    
+# N = int(input('Write number N: '))
+# if N == ' ':
+#     print('Incorrect number')
+# else:
+#     temp = 1
+#     lst1 = []
+#     lst1.append(temp) 
+#     for i in range(N - 1):
+#         temp *= -3
+#         lst1.append(temp)
+#     print(lst1)    
+
+# Возможно решение и с помощью функции
+
+N = int(input('Write number N: '))          # Вводим число N
+
+def get_list(N):                            # Создаем функцию получения списка из последовательности
+    return [((-3)**i) for i in range(N)]    # Возвращаем заполненный список
+print(get_list(N))                          # Вызываем функцию через print
+        
