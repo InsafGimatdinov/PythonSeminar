04174bc5e5223bfaa383e3cef7013181983429b3
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-04-04 23:57:40 +0300

    Add correct to this exaple file

--- 32.Get_list_from_sequence.py
+++ 32.Get_list_from_sequence.py
@@ -1,4 +1,4 @@
-# Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
+# Дана последовательность чисел. Получить список повторяющихся элементов исходной последовательности
 # Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
 
 numbers = [1, 2, 3, 5, 1, 5, 3, 10]   # Asked numbers list
@@ -6,16 +6,3 @@ numbers = [1, 2, 3, 5, 1, 5, 3, 10]   # Asked numbers list
 uniq_numbers = list(set(numbers))     # Used standart function set
 
 print(uniq_numbers)                   # Print result
-
-# ----------------------------------------------------------------------------------------------------
-
-import random                                                       # import module random
-
-sequence = [1, 2, 3, 5, 1, 5, 3, 10]                                # Asked list numbers
-
-result = list(filter(lambda x: sequence.count(x) == 1, sequence))   
-
-print('first sequence = {}'.format(sequence))                       # Print first sequence
-
-print('Second sequence = {}'.format(result))                        # Print result sequence
-
